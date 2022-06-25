e4e4002ab526a2301e34da784ad14115ba04c0c2
mhdelavega <huertadelavegamiguel@gmail.com>
2022-06-25 23:41:37 +0200

    FEAT: attention card

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className='card'>
+        <span className='logo-attention'></span>
+        <p>¡Cuidado!</p>
+        <p>No podrás volver atrás</p>
+        <button>Borrar</button>
+        <button>Cancelar</button>
+      </div>
+    </>
+
+
+  );
+}
+
+export default App;
