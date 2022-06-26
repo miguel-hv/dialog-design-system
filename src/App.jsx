7b0d24a9a14a6b852fe2b4c5eaf14f3f8dd3033c
mhdelavega <huertadelavegamiguel@gmail.com>
2022-06-26 19:11:29 +0200

    FEAT: attention card styles

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,14 @@ function App() {
   return (
     <>
       <div className='card'>
+        <span className='logo-close'></span>
         <span className='logo-attention'></span>
-        <p>¡Cuidado!</p>
-        <p>No podrás volver atrás</p>
-        <button>Borrar</button>
-        <button>Cancelar</button>
+        <h4 className='title'>¡Cuidado!</h4>
+        <p className='description'>No podrás volver atrás</p>
+        <div className='buttons-container'>
+          <button className='button button--contained'>Borrar</button>
+          <button className='button button--outlined'>Cancelar</button>
+        </div>
       </div>
     </>
 
